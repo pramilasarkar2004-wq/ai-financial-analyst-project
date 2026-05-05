b999570f33c5ddd9c4877ede62ecb32e70cbf6e2
pramilasarkar2004-wq <pramilasarkar2004@gmail.com>
2026-05-05 12:34:58 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
+
+app.post("/sentiment", (req, res) => {
+  res.json({ result: "AI is working ✅" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server started"));
